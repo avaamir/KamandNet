f1de4e388a0c1d6946600b6c378c47a7130031de
avaamir <avaamir>
2020-12-07 15:06:13 +0330

    Camera Util Added

--- app/src/main/java/mp/amir/ir/kamandnet/ui/activities/TestActivity.kt
+++ app/src/main/java/mp/amir/ir/kamandnet/ui/activities/TestActivity.kt
@@ -1,29 +1,21 @@
 package mp.amir.ir.kamandnet.ui.activities
 
 import android.content.Intent
-import android.graphics.Bitmap
-import android.graphics.BitmapFactory
 import android.os.Bundle
-import android.os.Environment
-import android.provider.MediaStore
-import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.content.FileProvider
 import kotlinx.android.synthetic.main.activity_test.*
-import mp.amir.ir.kamandnet.BuildConfig
 import mp.amir.ir.kamandnet.R
-import mp.amir.ir.kamandnet.utils.general.now
-import mp.amir.ir.kamandnet.utils.general.toast
-import okio.IOException
-import java.io.File
-import kotlin.math.max
-import kotlin.math.min
+import mp.amir.ir.kamandnet.utils.general.dispatchTakePictureIntent
+import mp.amir.ir.kamandnet.utils.general.setImageViewFromFile
 
 
 class TestActivity : AppCompatActivity() {
-    companion object {
+    private companion object {
         const val CAPTURE_IMAGE_REQ = 42
     }
+
+
+    private lateinit var currentPhotoPath: String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -32,11 +24,11 @@ class TestActivity : AppCompatActivity() {
         initViews()
     }
 
-    private lateinit var currentPhotoPath: String
-
     private fun initViews() {
         btnReqApi.setOnClickListener {
-            dispatchTakePictureIntent()
+            dispatchTakePictureIntent(CAPTURE_IMAGE_REQ)?.also {
+                currentPhotoPath = it.absolutePath
+            }
         }
     }
 
@@ -49,72 +41,4 @@ class TestActivity : AppCompatActivity() {
             }
         }
     }
-
-    @Throws(IOException::class)
-    private fun createImageFile(): File {
-        // Create an image file name
-        val storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES)!!
-        return File.createTempFile(
-            "JPEG_${now()}_", /* prefix */
-            ".jpg", /* suffix */
-            storageDir /* directory */
-        )
-    }
-
-    private fun dispatchTakePictureIntent() {
-        val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-
-        // Ensure that there's a camera activity to handle the intent
-        takePictureIntent.resolveActivity(packageManager)?.also {
-            // Create the File where the photo should go
-            val photoFile: File? = try {
-                createImageFile().also {
-                    currentPhotoPath = it.absolutePath
-                }
-            } catch (ex: IOException) {
-                // Error occurred while creating the File
-                toast(ex.message.toString())
-                null
-            }
-            // Continue only if the File was successfully created
-            photoFile?.also {
-                val photoURI = FileProvider.getUriForFile(
-                    this,
-                    "${BuildConfig.APPLICATION_ID}.provider",
-                    it
-                )
-                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI)
-                startActivityForResult(takePictureIntent, CAPTURE_IMAGE_REQ)
-            }
-
-        }
-    }
-
-    private fun setImageViewFromFile(path: String, imageView: ImageView) {
-        // Get the dimensions of the View
-        val targetW: Int = imageView.width
-        val targetH: Int = imageView.height
-
-        val bmOptions = BitmapFactory.Options().apply {
-            // Get the dimensions of the bitmap
-            inJustDecodeBounds = true
-
-            BitmapFactory.decodeFile(path, this)
-
-            val photoW = outWidth
-            val photoH = outHeight
-
-            // Determine how much to scale down the image
-            val scaleFactor = max(1, min(photoW / targetW, photoH / targetH))
-
-            // Decode the image file into a Bitmap sized to fill the View
-            inJustDecodeBounds = false
-            inSampleSize = scaleFactor
-            inPurgeable = true
-        }
-        BitmapFactory.decodeFile(path, bmOptions)?.also { bitmap ->
-            imageView.setImageBitmap(bitmap)
-        }
-    }
-
 }
---
+++ app/src/main/java/mp/amir/ir/kamandnet/utils/general/camera.kt
@@ -0,0 +1,79 @@
+package mp.amir.ir.kamandnet.utils.general
+
+import android.app.Activity
+import android.content.Intent
+import android.graphics.BitmapFactory
+import android.os.Environment
+import android.provider.MediaStore
+import android.widget.ImageView
+import androidx.core.content.FileProvider
+import mp.amir.ir.kamandnet.BuildConfig
+import mp.amir.ir.kamandnet.ui.activities.TestActivity
+import okio.IOException
+import java.io.File
+import kotlin.math.max
+import kotlin.math.min
+
+@Throws(IOException::class)
+private fun Activity.createImageFile(): File {
+    // Create an image file name
+    val storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES)!!
+    return File.createTempFile(
+        "JPEG_${now()}_", /* prefix */
+        ".jpg", /* suffix */
+        storageDir /* directory */
+    )
+}
+
+@Throws(IOException::class)
+fun Activity.dispatchTakePictureIntent(captureImageReqId: Int): File? {
+    val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+
+    // Ensure that there's a camera activity to handle the intent
+    takePictureIntent.resolveActivity(packageManager)?.also {
+        // Create the File where the photo should go
+        val photoFile = try {
+            createImageFile()
+        } catch (ex: IOException) {
+            // Continue only if the File was successfully created
+            throw ex
+        }
+
+        val photoURI = FileProvider.getUriForFile(
+            this,
+            "${BuildConfig.APPLICATION_ID}.provider",
+            photoFile
+        )
+        takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI)
+        startActivityForResult(takePictureIntent, captureImageReqId)
+        return photoFile
+    }
+    return null
+}
+
+fun setImageViewFromFile(path: String, imageView: ImageView) {
+    // Get the dimensions of the View
+    val targetW: Int = imageView.width
+    val targetH: Int = imageView.height
+
+    val bmOptions = BitmapFactory.Options().apply {
+        // Get the dimensions of the bitmap
+        inJustDecodeBounds = true
+
+        BitmapFactory.decodeFile(path, this)
+
+        val photoW = outWidth
+        val photoH = outHeight
+
+        // Determine how much to scale down the image
+        val scaleFactor = max(1, min(photoW / targetW, photoH / targetH))
+
+        // Decode the image file into a Bitmap sized to fill the View
+        inJustDecodeBounds = false
+        inSampleSize = scaleFactor
+        inPurgeable = true
+    }
+    BitmapFactory.decodeFile(path, bmOptions)?.also { bitmap ->
+        imageView.setImageBitmap(bitmap)
+    }
+}
